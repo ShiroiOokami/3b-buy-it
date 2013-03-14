ab94fbd865c95a76f5d8e037f4b3decf1d9be46f
Nathan Thomas <erom.shaman@gmail.com>
2013-03-14 15:45:30 -0400

    This includes my Reg Exps

---
+++ Best Book Buy Project/src/Main/UserRegExps.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class UserRegExps {
+	private String Name = "^\\w[\\w.]{0,19}$";
+	private String PIN = "^\\d{4}$";
+	private String StreetAdd = "";
+	private String ZIP = "^\\d{5}$";
+	private String State = "";
+	private String City = "";
+	
+	public boolean name(String n)
+	{
+		return false;
+	}
+}
