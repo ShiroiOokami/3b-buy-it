60ecf05c7c210bf7bfa39c4b4a705ac65d2ed8b6
Andrew <Andrew@164.76.64.235>
2013-04-11 16:05:16 -0400

    CardType comments

--- Best Book Buy Project/src/Main/CardType.java
+++ Best Book Buy Project/src/Main/CardType.java
@@ -1,5 +1,12 @@
 package Main;
 
+/**
+ * An enumerated type for all of the valid credit
+ * cards the users can use.
+ * 
+ * @version 2013-04-11
+ *
+ */
 public enum CardType {
 	MasterCard("MasterCard"),
 	VISA("VISA"),
